aa084adc71f29a3f9cd9dd86726227bb331efb7c
Adam W Berck <adamwberck@gmail.com>
2021-07-16 08:32:09 -0700

    init

--- ForLoop/src/RegexDemo.java
+++
@@ -1,8 +0,0 @@
-import java.math.BigInteger;
-
-public class RegexDemo {
-    public static void main(String[] args){
-        BigInteger large_hex = new BigInteger("a9046c73e00331af68917d3804f70655",16);
-        System.out.println(large_hex.remainder(new BigInteger("100")));
-    }
-}
